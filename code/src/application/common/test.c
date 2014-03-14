48412eef1e78892831f9a46192c878a969834c10
Renate Hansen <renate@renate.(none)>
2014-03-14 14:46:10 +0100

    added ggpio test

--- code/src/application/common/test.c
+++ code/src/application/common/test.c
@@ -10,9 +10,19 @@
 #include <flawless/core/msg_msgPump.h>
 #include <flawless/config/msgIDs.h>
 
+#include <libopencm3/stm32/f0/rcc.h>
+
+#include "libopencm3/stm32/gpio.h"
+
 static void test_init(void);
-MODULE_INIT_FUNCTION(test,NEVER, test_init);
+MODULE_INIT_FUNCTION(test,9, test_init);
 static void test_init(void)
 {
+	RCC_AHBENR |= RCC_AHBENR_GPIOAEN;
+	
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO6);
+
+	gpio_set(GPIOA, GPIO6);
+
 }
 
