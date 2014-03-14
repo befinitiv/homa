fff7a5ed5065e155d11ec4f0e7578729b5ac2460
Lutz <lutz.freitag@fu-berlin.de>
2014-03-14 12:33:16 +0100

    minor cleanup

--- code/build/arm/debug/application/obj/memory.d
+++
@@ -1,4 +0,0 @@
-build/arm/debug/application/obj/memory.o: src/target/stm32f0/memory.c \
- src/framework/include/flawless/init/systemInitializer.h
-
-src/framework/include/flawless/init/systemInitializer.h:
